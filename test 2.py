61b033f1e9acd43999655ed8cd39f0f3ce7c54f9
kitajusSus <116976902+kitajusSus@users.noreply.github.com>
2024-10-21 23:13:32 +0200

    Update test 2.py

--- test 2.py
+++ test 2.py
@@ -6,7 +6,7 @@ import time
 def rozpoznaj_slowa_z_pliku(nazwa_pliku, ile_slow_do_rozpoznania):
     r = sr.Recognizer()
     with sr.AudioFile(nazwa_pliku) as source:
-        audio = r.record(source)
+        audio = r.record(source) 
         try:
             text = r.recognize_google(audio, language="pl-PL")
             słowa = text.split()[:ile_slow_do_rozpoznania]
@@ -19,7 +19,7 @@ def powtorz_słowa(słowa):
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Powtórz słowa, które usłyszałeś:")
-        audio = r.listen(source)
+        audio = r.listen(source) #uruchamianie mikrofonu
         try:
             powtórzone_słowa = r.recognize_google(audio, language="pl-PL").split()
             poprawne_słowa = [s for s, ps in zip(słowa, powtórzone_słowa) if s.lower() == ps.lower()]
@@ -38,24 +38,24 @@ def main():
     nazwa_pliku = "test1_.wav"  # nazwa pliku
     ile_slow_do_rozpoznania = 5
     
-    # Wczytaj słowa z pliku
+    # WCZYTYWANIE SŁOW Z AUDIO DO MODELU 
     słowa = rozpoznaj_slowa_z_pliku(nazwa_pliku, ile_slow_do_rozpoznania)
     if not słowa:
         print("Nie udało się rozpoznać słów z pliku.")
         return
-    # Odtwórz audio dla użytkownika
+    # ODTWARZANIE AUDIO
     print("Odtwarzam audio...")
     odtwarzaj_audio(nazwa_pliku)
 
-    # Pozwól użytkownikowi powtórzyć słowa
+    # UZYTKOWNIK MÓWI
     print("Za chwile rozpocznie sie nagrywanie twojej odpowiedzi.")
     time.sleep(1)  # Krótka przerwa przed rozpoczęciem nagrywania
     poprawne_słowa, powtórzone_słowa = powtorz_słowa(słowa)
 
-    # Wyświetl słowa powiedziane poprawnie przez użytkownika
+    # Wyświetlanie słów powiedzianych poprawnie przez użytkownika
     print(f"\nPoprawnie powtórzone słowa: {' '.join(poprawne_słowa)}")
     
-    # Wyświetl wszystkie słowa z pliku
+    # wszystkie słowa z pliku
     print(f"Wszystkie słowa z pliku: {' '.join(słowa)}")
     print(f"Wynik: {len(poprawne_słowa)}/{ile_slow_do_rozpoznania}")
 
